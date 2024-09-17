3956b3488c19744b1098a654ff5298b90a54d2b5
kevinxz01@gmail.com <Kevinxz01@gmail.com>
2024-09-17 05:06:05 -0500

    First commit from coding challenges website.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+public class Program
+{
+	private const int _MAX_NUM_HOPS = 30;
+	private const string _packet_sending = "some random packet. mainly for testing the traceroute and not the packet itself.";
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+		Console.WriteLine("Traceroute command. Enter the host name for the traceroute command");
+		var hostname = Console.Read();
+
+		Console.WriteLine(hostname);
+	}
+}
